bfbcbc41189c5a2129836e4397d1985a7f7570e8
Kushal Roy Chowdhury <kushalr901@gmail.com>
2025-09-14 13:42:23 +0530

    ChatBuddy v1.2
    Version Number Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -695,7 +695,7 @@ export default function App() {
                 <div className="text-center text-gray-600">
                   AI can make mistakes.
                   <br />
-                  v1.1
+                  v1
                   <br />
                   By: KushalRoyChowdhury
                 </div>
