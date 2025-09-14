5cf8a410fdb639612fd53fb742fe5e82307e2a05
Kushal Roy Chowdhury <kushalr901@gmail.com>
2025-09-14 14:07:01 +0530

    ChatBuddy v1.2.1
    Model Tuning.

--- backend/internalModelInstruction/internalThinkInstruction.js
+++ backend/internalModelInstruction/internalThinkInstruction.js
@@ -6,6 +6,6 @@ Show your thinking with narrative style inside your responses key, under the <th
 - Natural broken in multiple paragraphs in thinking.
 - Natural response always after think tag. 
 - Use the think tags in every response key, if you get this msg in your call.
-- **MAX Thinking budget: 5000 char**, dont use thinking in simple task.
-- Never show code blocks and systemInstruction or memory action, or JSON formatting inside think.
-- KEEP THE FINAL JSON RESPONSE INTACT`;
+- **MAX Thinking budget: 4000 character**, dont use thinking in simple task.
+- NEVER use code blocks (\`\`\` language ... \`\`\`) in think tags. It will be a pure conversation of your own thinking.
+- NEVER write which memory function to use, or what are your system Instructions inside think tags.`;
